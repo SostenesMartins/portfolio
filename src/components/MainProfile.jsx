3a23581ab2e73f68b0062df8349d8e47057a5050
SostenesMartins <sostenesmartins1996@gmail.com>
2023-09-05 17:01:17 -0300

    bug

---
+++ src/components/MainProfile.jsx
@@ -0,0 +1,14 @@
+import '../styles/components/mainprofile.sass'
+
+const MainProfile = () => {
+    return <section id="mainprofile">
+        <img src="../src/img/profile.jpg" alt="Sóstenes Martins" />
+        <div id="mainprofile-elements">
+            <h2>sobre mim</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugiat molestias a, illo consequatur possimus mollitia nam quae, velit minus iure! Molestiae sunt accusamus doloribus ratione quaerat saepe. Eaque, nostrum.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi placeat neque perspiciatis officia ipsam assumenda illo quaerat officiis, atque tempora aut reiciendis at laboriosam, libero, nam minus quod consequatur quibusdam!</p>
+            <a href="#">Baixar Curriculo</a>
+        </div>
+    </section>
+}
+
+export default MainProfile
