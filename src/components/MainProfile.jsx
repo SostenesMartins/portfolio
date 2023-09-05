da35ce47f98da575e64d88f026e94423df561d92
SostenesMartins <sostenesmartins1996@gmail.com>
2023-09-05 17:08:04 -0300

    img bug

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -1,8 +1,10 @@
+import Avatar from '../img/profile.jpg'
+
 import '../styles/components/mainprofile.sass'
 
 const MainProfile = () => {
     return <section id="mainprofile">
-        <img src="../src/img/profile.jpg" alt="Sóstenes Martins" />
+        <img src={Avatar} alt="Sóstenes Martins" />
         <div id="mainprofile-elements">
             <h2>sobre mim</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugiat molestias a, illo consequatur possimus mollitia nam quae, velit minus iure! Molestiae sunt accusamus doloribus ratione quaerat saepe. Eaque, nostrum.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi placeat neque perspiciatis officia ipsam assumenda illo quaerat officiis, atque tempora aut reiciendis at laboriosam, libero, nam minus quod consequatur quibusdam!</p>
