959a7fc8f6e7a37ff68545cda50aaab19b6a1022
SostenesMartins <sostenesmartins1996@gmail.com>
2023-09-05 16:55:13 -0300

    main profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar"
+import MainProfile from "./components/MainProfile"
+
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <MainProfile />
     </>
   )
 }
--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,15 +3,15 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from "react-icons/fa"
 import "../styles/components/socialnetworks.sass"
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn/>},
-    {name: "github", icon: <FaGithub />},
-    {name: "instagram", icon: <FaInstagram/>},
+    {name: "linkedin", icon: <FaLinkedinIn/>, href: "https://www.linkedin.com/in/s%C3%B3stenes-de-souza-siqueira-martins-336b491bb/"},
+    {name: "github", icon: <FaGithub />, href: "https://github.com/SostenesMartins"},
+    {name: "instagram", icon: <FaInstagram/>, href: "https://www.instagram.com/"},
 ]
 
 const SocialNetworks = () => {
     return <section id="social-networks">
         {socialNetworks.map((networks) => (
-            <a href="#" className="social-link" id={networks.name} key={networks.name}>
+            <a href={networks.href} target="_blank" className="social-link" id={networks.name} key={networks.name}>
                 {networks.icon}
             </a>
         ))}
